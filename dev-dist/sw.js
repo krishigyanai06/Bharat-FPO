87c84946e7af130312b38a281c24436a6cfe401f
Amar Kaygude <amarkaygude09@gmail.com>
2026-08-06 20:03:34 +0530

    fixed issue of tenant code

--- dev-dist/sw.js
+++ dev-dist/sw.js
@@ -82,7 +82,7 @@ define(['./workbox-f87553f6'], (function (workbox) { 'use strict';
     "revision": "3ca0b8505b4bec776b69afdba2768812"
   }, {
     "url": "index.html",
-    "revision": "0.giu9cen43io"
+    "revision": "0.rpng7a3qgc8"
   }], {});
   workbox.cleanupOutdatedCaches();
   workbox.registerRoute(new workbox.NavigationRoute(workbox.createHandlerBoundToURL("index.html"), {
